9275a35d5d5ded4b95eb879a270c32cde33a2c6f
Marcos <marcos.ledesma.picazo@cieep.com>
2021-03-04 20:50:42 +0100

    Terminado

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -14,11 +14,41 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
 
-  constructor() { }
+  constructor() {
+    console.log("constructor");
+  }
 
   ngOnInit(): void {
+  }
+
+  ngOnChanges(): void {
+    console.log("ngOnChanges");
+  }
+
+  ngDoCheck(): void {
+    console.log("ngDoCheck");
+  }
+
+  ngAfterContentInit(): void {
+    console.log("ngAfterContentInit");
+  }
+
+  ngAfterContentChecked(): void {
+    console.log("ngAfterContentChecked");
+  }
+
+  ngAfterViewInit(): void {
+    console.log("ngAfterViewInit");
+  }
+
+  ngAfterViewChecked(): void {
+    console.log("ngAfterViewChecked");
+  }
+
+  ngOnDestroy(): void {
+    console.log("ngOnDestroy");
   }
 
 }
